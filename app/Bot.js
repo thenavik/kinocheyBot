70e0259f5d253cd9d361ae88c8475b79e2e478e1
gholib <gtrandnet@gmail.com>
2022-04-03 03:14:27 +0500

    fix

--- app/Bot.js
+++ app/Bot.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api')
-const {token} = process.env.BOT_TOKEN
+const token = process.env.BOT_TOKEN
 const StartHandler = require('./handlers/StartHandler')
 const url =  process.env.APP_URL
 
